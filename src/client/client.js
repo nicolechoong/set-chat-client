2b9d5224c07394fa51549b3eda533b6905971039
nicole <nwyc2@cam.ac.uk>
2022-11-02 16:14:56 +0000

    something

--- src/client/client.js
+++ src/client/client.js
@@ -12,12 +12,23 @@ var localUsername;
 
 const configuration = { 
    "iceServers": [
-    { "urls": "stun:stun.12connect.com:3478" },
-    {
-        "urls": "turn:openrelay.metered.ca:443?transport=tcp",
-        "username": "openrelayproject",
-        "credential": "openrelayproject",
-    }
+        { "urls": "stun:stun.12connect.com:3478" },
+        { "urls": "stun:openrelay.metered.ca:80" },
+        {
+            "urls": "turn:openrelay.metered.ca:80",
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
+        {
+            "urls": "turn:openrelay.metered.ca:443",
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        },
+        {
+            "urls": "turn:openrelay.metered.ca:443?transport=tcp",
+            "username": "openrelayproject",
+            "credential": "openrelayproject",
+        }
     ] 
 }; 
 
