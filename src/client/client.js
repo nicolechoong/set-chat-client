7c57e1b1aef576af974aac9e0ef7e59790ec5b3e
nicole <nwyc2@cam.ac.uk>
2023-01-09 02:11:50 +0000

    debugging shit

--- src/client/client.js
+++ src/client/client.js
@@ -485,11 +485,12 @@ function verifyOperations (ops) {
     
     // only one create
     ops = [...ops];
+    console.log(ops.length);
     const createOps = ops.filter((op) => op.action === "create");
     if (createOps.length != 1) { console.log("op verification failed: more than one create"); return false; }
     const createOp = createOps[0];
-    console.log(`createops ${createOp.sig}`);
-    console.log(`${createOp.sig instanceof Uint8Array}     ${createOp.pk instanceof Uint8Array}`)
+    console.log(`createops ${createOp.pk}`);
+    console.log(`${typeof createOp.sig}     ${createOp.pk instanceof Uint8Array}`)
     if (!nacl.sign.detached.verify(enc.encode(concatOp(createOp)), createOp.sig, createOp.pk)) { console.log("op verification failed: create key verif failed"); return false; }
 
     const otherOps = ops.filter((op) => {return op.action !== "create"});
