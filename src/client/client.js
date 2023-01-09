787663a5609002377a72f699d6b7acf8f0b7f7f6
nicole <nwyc2@cam.ac.uk>
2023-01-09 22:44:50 +0000

    uint8array

--- src/client/client.js
+++ src/client/client.js
@@ -497,7 +497,7 @@ function verifyOperations (ops) {
     if (createOps.length != 1) { console.log("op verification failed: more than one create"); return false; }
     const createOp = createOps[0];
     console.log(createOp.sig);
-    console.log(`${typeof createOp.sig}     ${enc.encode(createOp.pk) instanceof Uint8Array}`)
+    console.log(`${Uint8Array.from(createOp.sig) instanceof Uint8Array}     ${enc.encode(createOp.pk) instanceof Uint8Array}`)
     console.log(`sig length ${createOp.sig.length}`);
     if (!nacl.sign.detached.verify(enc.encode(concatOp(createOp)), createOp.sig instanceof Uint8Array ? createOp.sig : Uint8Array.from(createOp.sig), createOp.pk)) { console.log("op verification failed: create key verif failed"); return false; }
 
