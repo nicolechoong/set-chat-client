e64ba3e401f60d9e167a389d428a31245629abcf
nicole <nwyc2@cam.ac.uk>
2023-01-09 22:47:03 +0000

    what am i printing

--- src/client/client.js
+++ src/client/client.js
@@ -496,7 +496,7 @@ function verifyOperations (ops) {
     console.log(JSON.stringify(ops));
     if (createOps.length != 1) { console.log("op verification failed: more than one create"); return false; }
     const createOp = createOps[0];
-    console.log(createOp.sig);
+    console.log(Uint8Array.from(createOp.sig));
     console.log(`${Uint8Array.from(createOp.sig) instanceof Uint8Array}     ${enc.encode(createOp.pk) instanceof Uint8Array}`)
     console.log(`sig length ${Uint8Array.from(createOp.sig).length}`);
     if (!nacl.sign.detached.verify(enc.encode(concatOp(createOp)), Uint8Array.from(createOp.sig), createOp.pk)) { console.log("op verification failed: create key verif failed"); return false; }
