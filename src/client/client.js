84303640127dc46d4d989f0d7636f69a5d699f76
nicole <nwyc2@cam.ac.uk>
2023-01-09 23:40:09 +0000

    trying import

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,6 @@
 import localforage from "https://unpkg.com/localforage@1.9.0/src/localforage.js";
 import nacl from "tweetnacl";
+import naclUtil from "tweetnacl-util";
 
 var loginBtn = document.getElementById('loginBtn'); 
 var sendMessageBtn = document.getElementById('sendMessageBtn');
@@ -250,7 +251,7 @@ async function onCreateChat (chatID, chatName, validMemberPubKeys, invalidMember
     
     for (const mem of validMemberPubKeys.keys()) {
         console.log(`adding ${mem} with pk ${validMemberPubKeys.get(mem)} to keyMap`);
-        keyMap.set(mem, nacl.util.encodeUTF8(validMemberPubKeys.get(mem)));
+        keyMap.set(mem, naclUtil.encodeUTF8(validMemberPubKeys.get(mem)));
     }
     
     if (invalidMembers.length > 0) {
@@ -308,7 +309,7 @@ async function addToChat(validMemberPubKeys, chatID) {
 
                 const sentTime = Date.now();
                 broadcastToMembers({
-                    id: nacl.hash(nacl.util.encodeUTF8(`${localUsername}:${sentTime}`)),
+                    id: nacl.hash(naclUtil.encodeUTF8(`${localUsername}:${sentTime}`)),
                     type: "add",
                     op: op,
                     from: localUsername,
@@ -337,7 +338,7 @@ async function addToChat(validMemberPubKeys, chatID) {
 function getDeps (operations) {
     var deps = new Set();
     for (const op of operations) {
-        const hashedOp = nacl.hash(nacl.util.encodeUTF8(concatOp(op)));
+        const hashedOp = nacl.hash(naclUtil.encodeUTF8(concatOp(op)));
         if (op.action !== "create" && !op.deps.has(hashedOp)) {
             deps.add(hashedOp);
             console.log(`dependency ${op.pk1} ${op.action} ${op.pk2}`);
@@ -370,8 +371,8 @@ async function generateOp (action, chatID, pk2 = null, ops = new Set()) {
                 deps: Array.from(getDeps(ops))
             };
         }
-        console.log(`encoded ${nacl.util.encodeUTF8(concatOp(op)) instanceof Uint8Array}, length of sig ${nacl.sign.detached(new TextEncoder().encode(concatOp(op)), keyPair.secretKey).length}`);
-        op["sig"] = nacl.sign.detached(nacl.util.encodeUTF8(concatOp(op)), keyPair.secretKey);
+        console.log(`encoded ${naclUtil.encodeUTF8(concatOp(op)) instanceof Uint8Array}, length of sig ${nacl.sign.detached(new TextEncoder().encode(concatOp(op)), keyPair.secretKey).length}`);
+        op["sig"] = nacl.sign.detached(naclUtil.encodeUTF8(concatOp(op)), keyPair.secretKey);
             resolve(op);
     });
 }
@@ -381,7 +382,7 @@ async function sendOperations (chatID, username) {
     store.getItem(chatID).then((chatInfo) => {
         const stringedOps = [...chatInfo.metadata.operations].map(op => {
             console.log(op.sig);
-            op.sig = nacl.util.decodeUTF8(op.sig);
+            op.sig = naclUtil.decodeUTF8(op.sig);
             console.log(op.sig);
             return op; 
         });
@@ -398,7 +399,7 @@ async function receivedOperations (ops, chatID, username) {
     // ops: array of operation objectss
     console.log(`receiving operations`);
     store.getItem(chatID).then((chatInfo) => {
-        ops = ops.map(op => { console.log(op.sig); op.sig = nacl.util.encodeUTF8(op.sig); return op; });
+        ops = ops.map(op => { console.log(op.sig); op.sig = naclUtil.encodeUTF8(op.sig); return op; });
         ops = new Set([...chatInfo.metadata.operations, ...ops]);
         console.log(`verified ${verifyOperations(ops)} is member ${members(ops, chatInfo.metadata.ignored).has(keyMap.get(username))}`);
         if (verifyOperations(ops) && members(ops, chatInfo.metadata.ignored).has(keyMap.get(username))) {
@@ -412,7 +413,7 @@ async function receivedOperations (ops, chatID, username) {
 function getOpFromHash(ops, hashedOp) {
     if (hashedOps.has(hashedOp)) { return hashedOps.get(hashedOp); }
     for (const op of ops) {
-        if (hashedOp == nacl.hash(nacl.util.encodeUTF8(concatOp(op)))) {
+        if (hashedOp == nacl.hash(naclUtil.encodeUTF8(concatOp(op)))) {
             hashedOps.set(hashedOp, op);
             return op;
         }
@@ -505,18 +506,18 @@ function verifyOperations (ops) {
     if (createOps.length != 1) { console.log("op verification failed: more than one create"); return false; }
     const createOp = createOps[0];
     console.log(createOp.sig);
-    console.log(`${typeof createOp.sig}     ${nacl.util.encodeUTF8(createOp.pk) instanceof Uint8Array}`)
+    console.log(`${typeof createOp.sig}     ${naclUtil.encodeUTF8(createOp.pk) instanceof Uint8Array}`)
     console.log(`sig length ${createOp.sig.length}`);
     if (!nacl.sign.detached.verify(new TextEncoder().encode(concatOp(createOp)), createOp.sig, createOp.pk)) { console.log("op verification failed: create key verif failed"); return false; }
 
     const otherOps = ops.filter((op) => {return op.action !== "create"});
-    const hashedOps = new Set(ops.map((op) => nacl.hash(nacl.util.encodeUTF8(JSON.stringify(op)))));
+    const hashedOps = new Set(ops.map((op) => nacl.hash(naclUtil.encodeUTF8(JSON.stringify(op)))));
 
     for (const op of otherOps) {
         // valid signature
-        console.log(`${op.sig instanceof Uint8Array}     ${nacl.util.encodeUTF8(op.pk1) instanceof Uint8Array}`)
+        console.log(`${op.sig instanceof Uint8Array}     ${naclUtil.encodeUTF8(op.pk1) instanceof Uint8Array}`)
         console.log(`sig length ${createOp.sig.length}`);
-        if (!nacl.sign.detached.verify(nacl.util.encodeUTF8(concatOp(op)), op.sig, op.pk1)) { console.log("op verification failed: key verif failed"); return false; }
+        if (!nacl.sign.detached.verify(naclUtil.encodeUTF8(concatOp(op)), op.sig, op.pk1)) { console.log("op verification failed: key verif failed"); return false; }
 
         // non-empty deps and all hashes in deps resolve to an operation in o
         for (const dep of op.deps) {
@@ -678,7 +679,7 @@ function sendChatMessage (messageInput) {
     console.log("message sent");
     const sentTime = Date.now();
     const data = {
-        id: nacl.hash(nacl.util.encodeUTF8(`${localUsername}:${sentTime}`)),
+        id: nacl.hash(naclUtil.encodeUTF8(`${localUsername}:${sentTime}`)),
         type: "text",
         from: localUsername,
         message: messageInput,
@@ -707,7 +708,7 @@ loginBtn.addEventListener("click", function (event) {
         sendToServer({ 
             type: "login", 
             name: loginInput,
-            pubKey: nacl.util.decodeUTF8(keyPair.publicKey)
+            pubKey: naclUtil.decodeUTF8(keyPair.publicKey)
         });
     }
 });
