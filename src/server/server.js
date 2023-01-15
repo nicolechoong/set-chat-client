c051b8ef3caf0ce68de6902f6533c63a3db4c55a
nicole <nwyc2@cam.ac.uk>
2023-01-15 20:59:23 +0000

    oops one more

--- src/server/server.js
+++ src/server/server.js
@@ -313,7 +313,7 @@ function onGetPK (connection, data) {
 function onAdd (connection, data) {
   // data = {type: 'add', to: username of invited user, chatID: chat id}
   console.log(`sending add message for chat ${data.chatID} to ${allUsers.get(objToStr(data.to)).username}, with public key ${JSON.stringify(data.to)}`);
-  sendTo(connectedUsers.get(JSON.stringify(data.to)).connection, data);
+  sendTo(connectedUsers.get(sendTo(data.to)).connection, data);
 }
 
 function broadcastActiveUsernames () {
