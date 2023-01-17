be90ab4da319be510b4801a638c9be8b2aeadfc5
nicole <nwyc2@cam.ac.uk>
2023-01-17 18:27:39 +0000

    importing trials...

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,5 @@
 import localforage from "https://unpkg.com/localforage@1.9.0/src/localforage.js";
-import * as automerge from "https://unpkg.com/automerge@0.7.2/";
+import * as Automerge from "../../node_modules/@automerge/automerge";
 
 var loginBtn = document.getElementById('loginBtn'); 
 var sendMessageBtn = document.getElementById('sendMessageBtn');
