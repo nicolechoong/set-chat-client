a5d47e7d0220dc673a97af1f5d7e5864b9d4eb9d
nicole <nwyc2@cam.ac.uk>
2023-03-23 02:11:20 +0000

    debug

--- src/client/components.js
+++ src/client/components.js
@@ -95,10 +95,13 @@ export function generateUserCard (pk, username) {
     card.appendChild(h3);
 
     const button = document.createElement("button");
-    button.class = "removeUserBtn";
+    button.className = "removeUserBtn";
     button.onclick = (async () => {
         removeFromChat(new Map([[username, pk]]), currentChatID);
+        console.log(`click`);
     });
+    console.log(`set onclick`);
+    card.appendChild(button);
 
     const path = document.createElement("path");
     path.d = "M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM471 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z";
@@ -107,7 +110,6 @@ export function generateUserCard (pk, username) {
     svg.viewBox = "0 0 640 512";
     svg.appendChild(path);
     button.appendChild(svg);
-    card.appendChild(button);
 
     return card;
 }
