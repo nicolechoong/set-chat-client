eb49ff5b150abbe92d07c3bee4d743e2a67234d4
nicole <nwyc2@cam.ac.uk>
2023-04-28 22:16:09 +0100

    debug9

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,6 @@
 import localforage from "https://unpkg.com/localforage@1.9.0/src/localforage.js";
 import nacl from '../../node_modules/tweetnacl-es6/nacl-fast-es.js';
-import { v4 as uuidv4 } from "https://unpkg.com/browse/uuid@8.3.2/dist/index.js";
+import { v4 as uuidv4 } from 'https://cdn.jsdelivr.net/npm/uuid@9.0.0/+esm'
 import * as access from "./accessControl.js";
 import * as elem from "./components.js";
 import {strToArr, concatArr, formatDate, isAlphanumeric, arrToStr} from "./utils.js";
