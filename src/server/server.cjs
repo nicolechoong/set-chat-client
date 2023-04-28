45627dc7e28937dd967344bb2d9b79888c8e17be
nicole <nwyc2@cam.ac.uk>
2023-04-28 21:55:06 +0100

    exposing file

--- src/server/server.cjs
+++ src/server/server.cjs
@@ -43,6 +43,11 @@ app.get('/node_modules/tweetnacl-es6/nacl-fast-es.js', (req, res, next) => {
   res.status(200).sendFile(path.join(__dirname, '..', '..', 'node_modules', 'tweetnacl-es6', 'nacl-fast-es.js'));
 });
 
+app.get('/node_modules/uuid/dist/v4.js', (req, res, next) => {
+  console.log(`imported nacl-fast`);
+  res.status(200).sendFile(path.join(__dirname, '..', '..', 'node_modules', 'tweetnacl-es6', 'nacl-fast-es.js'));
+});
+
 const server = https.createServer({ key, cert }, app);
 
 const port = 3000;
